db42a8acf53b17d3df7aae5dfd7203beed509ddd
TsarF <ninja11103@gmail.com>
2023-09-02 17:24:18 -0700

    Initial commit

---
+++ StarTrackerUtility/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace StarTrackerUtility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonYP = new System.Windows.Forms.Button();
+            this.buttonYN = new System.Windows.Forms.Button();
+            this.buttonXN = new System.Windows.Forms.Button();
+            this.buttonXP = new System.Windows.Forms.Button();
+            this.buttonEnable = new System.Windows.Forms.Button();
+            this.buttonDisable = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBoxSerial = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.TrackerTimer = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxReverse = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonYP
+            // 
+            this.buttonYP.Location = new System.Drawing.Point(68, 12);
+            this.buttonYP.Name = "buttonYP";
+            this.buttonYP.Size = new System.Drawing.Size(50, 50);
+            this.buttonYP.TabIndex = 0;
+            this.buttonYP.Text = "Y+";
+            this.buttonYP.UseVisualStyleBackColor = true;
+            this.buttonYP.Click += new System.EventHandler(this.buttonYP_Click);
+            // 
+            // buttonYN
+            // 
+            this.buttonYN.Location = new System.Drawing.Point(68, 68);
+            this.buttonYN.Name = "buttonYN";
+            this.buttonYN.Size = new System.Drawing.Size(50, 50);
+            this.buttonYN.TabIndex = 1;
+            this.buttonYN.Text = "Y-";
+            this.buttonYN.UseVisualStyleBackColor = true;
+            this.buttonYN.Click += new System.EventHandler(this.buttonYN_Click);
+            // 
+            // buttonXN
+            // 
+            this.buttonXN.Location = new System.Drawing.Point(12, 41);
+            this.buttonXN.Name = "buttonXN";
+            this.buttonXN.Size = new System.Drawing.Size(50, 50);
+            this.buttonXN.TabIndex = 2;
+            this.buttonXN.Text = "X-";
+            this.buttonXN.UseVisualStyleBackColor = true;
+            this.buttonXN.Click += new System.EventHandler(this.buttonXN_Click);
+            // 
+            // buttonXP
+            // 
+            this.buttonXP.Location = new System.Drawing.Point(124, 41);
+            this.buttonXP.Name = "buttonXP";
+            this.buttonXP.Size = new System.Drawing.Size(50, 50);
+            this.buttonXP.TabIndex = 3;
+            this.buttonXP.Text = "X+";
+            this.buttonXP.UseVisualStyleBackColor = true;
+            this.buttonXP.Click += new System.EventHandler(this.buttonXP_Click);
+            // 
+            // buttonEnable
+            // 
+            this.buttonEnable.Location = new System.Drawing.Point(9, 210);
+            this.buttonEnable.Name = "buttonEnable";
+            this.buttonEnable.Size = new System.Drawing.Size(165, 50);
+            this.buttonEnable.TabIndex = 4;
+            this.buttonEnable.Text = "Enable Tracking";
+            this.buttonEnable.UseVisualStyleBackColor = true;
+            this.buttonEnable.Click += new System.EventHandler(this.buttonEnable_Click);
+            // 
+            // buttonDisable
+            // 
+            this.buttonDisable.Location = new System.Drawing.Point(9, 266);
+            this.buttonDisable.Name = "buttonDisable";
+            this.buttonDisable.Size = new System.Drawing.Size(165, 50);
+            this.buttonDisable.TabIndex = 5;
+            this.buttonDisable.Text = "Disable Tracking";
+            this.buttonDisable.UseVisualStyleBackColor = true;
+            this.buttonDisable.Click += new System.EventHandler(this.buttonDisable_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(9, 322);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(165, 50);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "Reset Position";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(180, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(512, 388);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBoxSerial
+            // 
+            this.comboBoxSerial.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBoxSerial.FormattingEnabled = true;
+            this.comboBoxSerial.Location = new System.Drawing.Point(9, 378);
+            this.comboBoxSerial.Name = "comboBoxSerial";
+            this.comboBoxSerial.Size = new System.Drawing.Size(164, 21);
+            this.comboBoxSerial.TabIndex = 8;
+            this.comboBoxSerial.SelectedIndexChanged += new System.EventHandler(this.comboBoxSerial_SelectedIndexChanged);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonConnect.Location = new System.Drawing.Point(9, 407);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(165, 22);
+            this.buttonConnect.TabIndex = 9;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(180, 408);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(412, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Location = new System.Drawing.Point(598, 407);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(94, 22);
+            this.buttonSend.TabIndex = 11;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // TrackerTimer
+            // 
+            this.TrackerTimer.Interval = 50;
+            this.TrackerTimer.Tick += new System.EventHandler(this.TrackerTimer_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(10, 143);
+            this.trackBar1.Maximum = 7;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(164, 45);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "StepSize: 0.1*";
+            // 
+            // checkBoxReverse
+            // 
+            this.checkBoxReverse.AutoSize = true;
+            this.checkBoxReverse.Location = new System.Drawing.Point(12, 187);
+            this.checkBoxReverse.Name = "checkBoxReverse";
+            this.checkBoxReverse.Size = new System.Drawing.Size(76, 17);
+            this.checkBoxReverse.TabIndex = 14;
+            this.checkBoxReverse.Text = "Reverse X";
+            this.checkBoxReverse.UseVisualStyleBackColor = true;
+            this.checkBoxReverse.CheckedChanged += new System.EventHandler(this.checkBoxReverse_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.checkBoxReverse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxSerial);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonDisable);
+            this.Controls.Add(this.buttonEnable);
+            this.Controls.Add(this.buttonXP);
+            this.Controls.Add(this.buttonXN);
+            this.Controls.Add(this.buttonYN);
+            this.Controls.Add(this.buttonYP);
+            this.MinimumSize = new System.Drawing.Size(420, 420);
+            this.Name = "Form1";
+            this.Text = "Star Tracker Utility";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonYP;
+        private System.Windows.Forms.Button buttonYN;
+        private System.Windows.Forms.Button buttonXN;
+        private System.Windows.Forms.Button buttonXP;
+        private System.Windows.Forms.Button buttonEnable;
+        private System.Windows.Forms.Button buttonDisable;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBoxSerial;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Timer TrackerTimer;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxReverse;
+    }
+}
+
---
+++ StarTrackerUtility/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StarTrackerUtility
+{
+    public partial class Form1 : Form
+    {
+        string selected_serial = "";
+        SerialPort serial = new SerialPort();
+
+        double currentX = 0.0;
+        double currentY = 0.0;
+
+        double[] stepSizes = { 0.01, 0.1, 0.2, 0.5, 1, 5, 10, 15 };
+        double stepSize = 0.1;
+
+        double Xdir = 1.0;
+
+        void UpdateSerial()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            comboBoxSerial.Items.Clear();
+            comboBoxSerial.Items.AddRange(ports);
+        }
+
+        void sendSerial(string text, string author = "HOST")
+        {
+            try
+            {
+                serial.WriteLine(text);
+                WriteLog(text, author);
+            }
+            catch (Exception ex)
+            {
+                WriteLog(ex.Message);
+            }
+        }
+
+        void UpdateTarget(double feedrate = 0.25)
+        {
+            sendSerial("G1 X" + currentX + " Y" + currentY + " F" + feedrate, "HOST");
+        }
+
+        void TickTarget(double duration)
+        {
+            double increment = 0.25 / 60 * (TimeSpan.FromSeconds(duration)).Ticks / TimeSpan.TicksPerSecond;
+            currentX += increment * 100 * Xdir;
+            UpdateTarget(25);
+        }
+
+        void TickTarget()
+        {
+            double increment = 0.25 / 60 * (DateTime.Now - lastUpdate).Ticks / TimeSpan.TicksPerSecond;
+            currentX += increment * 100 * Xdir;
+            UpdateTarget(25);
+        }
+
+        void WriteLog(string text, string author = "System")
+        {
+            richTextBox1.Invoke(new MethodInvoker(delegate { richTextBox1.AppendText("\n" + author + ": " + text); }));
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateSerial();
+            if (comboBoxSerial.Items.Count != 0) comboBoxSerial.SelectedIndex = 0;
+        }
+
+        private void buttonYP_Click(object sender, EventArgs e)
+        {
+            currentY += stepSize;
+            UpdateTarget(360);
+        }
+
+        private void buttonXP_Click(object sender, EventArgs e)
+        {
+            currentX += stepSize;
+            UpdateTarget(360);
+        }
+
+        private void buttonYN_Click(object sender, EventArgs e)
+        {
+            currentY -= stepSize;
+            UpdateTarget(360);
+        }
+
+        private void buttonXN_Click(object sender, EventArgs e)
+        {
+            currentX -= stepSize;
+            UpdateTarget(360);
+        }
+
+        private void buttonEnable_Click(object sender, EventArgs e)
+        {
+            lastUpdate = DateTime.Now;
+            TickTarget(1.0);
+            Thread.Sleep(1);
+            TrackerTimer.Start();
+        }
+
+        private void buttonDisable_Click(object sender, EventArgs e)
+        {
+            lastUpdate = DateTime.Now;
+            TrackerTimer.Stop();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            sendSerial("G92 X0 Y0");
+            currentX = 0;
+            currentY = 0;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if(serial.IsOpen)
+            {
+                sendSerial(textBox1.Text, "USER");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serial = new SerialPort(selected_serial, 115200);
+                serial.RtsEnable = true;
+                serial.DtrEnable = true;
+                serial.NewLine = "\n";
+                serial.Open();
+                if (serial.IsOpen)
+                {
+                    WriteLog("Opened serial port " + selected_serial);
+                    serial.WriteLine("M115");
+                    serial.DataReceived += new SerialDataReceivedEventHandler(Serial_Data_Received);
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLog("Failed opening serial port: " + ex.Message);
+            }
+        }
+
+        private void comboBoxSerial_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateSerial();
+            selected_serial = comboBoxSerial.Text;
+        }
+
+        void Serial_Data_Received(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort port = ((SerialPort)sender);
+            while (port.BytesToRead != 0)
+            {
+                WriteLog(port.ReadLine(), ((SerialPort)sender).PortName);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            stepSize = stepSizes[trackBar1.Value];
+            label1.Text = "Step Size: " + stepSize.ToString() + "*";
+        }
+
+        DateTime lastUpdate = DateTime.Now;
+        private void TrackerTimer_Tick(object sender, EventArgs e)
+        {
+            TickTarget();
+            lastUpdate = DateTime.Now;
+        }
+
+        private void checkBoxReverse_CheckedChanged(object sender, EventArgs e)
+        {
+            Xdir = checkBoxReverse.Checked ? -1.0 : 1.0;
+        }
+    }
+}
